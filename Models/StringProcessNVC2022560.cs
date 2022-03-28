7525260ef14bf2ea66d733f30c4663342944c484
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2022-03-28 07:39:08 +0700

    Create_Class_StrPro_NVC560_2022

---
+++ Models/StringProcessNVC2022560.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace NguyenVanCuong2022560
+{
+    public class StringProcessNVC2022560
+    {
+        public string UpperToLower (string StrIn)
+        {
+            string StrOut ="";
+            StrOut = StrIn.ToLower();
+            return StrOut;
+        }
+    }
+}
