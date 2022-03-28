1f29adacbc50bf1ddbbc90231ccf121563674042
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2022-03-28 07:12:42 +0700

    First_commit_NVC560_2022

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenVanCuong2022560.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
